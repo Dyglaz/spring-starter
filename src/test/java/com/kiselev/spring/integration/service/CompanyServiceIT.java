ce696631394ec0346482eb44bebaf0f63ed24e51
Dyglaz <kallkae@mail.ru>
2025-01-14 13:57:02 +0300

    test: update integration test for CompanyService

--- src/test/java/com/kiselev/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/kiselev/spring/integration/service/CompanyServiceIT.java
@@ -1,16 +1,21 @@
 package com.kiselev.spring.integration.service;
 
 import com.kiselev.spring.ApplicationRunner;
+import com.kiselev.spring.config.DatabaseProperties;
 import com.kiselev.spring.database.entity.Company;
 import com.kiselev.spring.dto.CompanyReadDto;
+import com.kiselev.spring.integration.annotation.IT;
 import com.kiselev.spring.listener.entity.EntityEvent;
 import com.kiselev.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -21,15 +26,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest()
+@IT
+@RequiredArgsConstructor()
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    private CompanyService companyService;
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
