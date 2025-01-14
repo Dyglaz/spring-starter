2ddd0522aaf2b1670660d1fe4d5972d649a68df5
Dyglaz <kallkae@mail.ru>
2025-01-14 14:32:37 +0300

    test: add TestApplicationRunner for integration tests configuration

---
+++ src/test/java/com/kiselev/spring/integration/TestApplicationRunner.java
@@ -0,0 +1,13 @@
+package com.kiselev.spring.integration;
+
+import com.kiselev.spring.database.pool.ConnectionPool;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+@TestConfiguration
+public class TestApplicationRunner {
+
+    @SpyBean(name = "pool1")
+    private ConnectionPool pool1;
+
+}
